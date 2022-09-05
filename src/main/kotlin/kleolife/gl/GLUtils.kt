bbf3e98ab784845e607265df3f8787787447da87
Laurent Laborde <kerdezixe@gmail.com>
2022-09-05 22:17:38 +0200

    GitHub Copilot is reading my mind

--- src/main/kotlin/kleolife/gl/GLUtils.kt
+++ src/main/kotlin/kleolife/gl/GLUtils.kt
@@ -1,7 +1,9 @@
 package kleolife.gl
 
 import kleolife.data.GraphicData
+import org.khronos.webgl.WebGLProgram
 import org.khronos.webgl.WebGLRenderingContext
+import org.khronos.webgl.WebGLShader
 
 class GLUtils {
     companion object {
@@ -11,6 +13,22 @@ class GLUtils {
             gfx.ctx.clearColor(0.0f, 0.0f, 0.0f, 1.0f)
             gfx.ctx.clear(WebGLRenderingContext.COLOR_BUFFER_BIT)
         }
+
+        fun createShader(gfx: GraphicData, type: Int, source: String): WebGLShader? {
+            val shader = gfx.ctx.createShader(type)
+            gfx.ctx.shaderSource(shader, source)
+            gfx.ctx.compileShader(shader)
+            return shader
+        }
+
+        fun createProgram(gfx: GraphicData, vertexShader: WebGLShader, fragmentShader: WebGLShader): WebGLProgram? {
+            val program = gfx.ctx.createProgram()
+            gfx.ctx.attachShader(program, vertexShader)
+            gfx.ctx.attachShader(program, fragmentShader)
+            gfx.ctx.linkProgram(program)
+            return program
+        }
+
     }
 }
 
