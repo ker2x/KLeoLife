1fa6e58444c60b3f7f4e93ca69ce58ec1370c132
Laurent Laborde <kerdezixe@gmail.com>
2022-09-05 17:33:54 +0200

    cleanup

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,8 +1,5 @@
-import kotlinx.html.div
-import kotlinx.html.dom.append
 import kotlinx.browser.document
 import kotlinx.browser.window
-import kotlinx.html.canvas
 import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLCanvasElement
 
@@ -23,12 +20,8 @@ fun setup() {
     gfx.canvas.height = gfx.height
     document.body!!.append(gfx.canvas)
 
-//    gfx.canvas = document.getElementById("canvas") as HTMLCanvasElement
     gfx.ctx = gfx.canvas.getContext("2d") as CanvasRenderingContext2D
 
-    console.log("canvas created")
-    console.log("canvas width: ${gfx.width}")
-    console.log("canvas height: ${gfx.height}")
     // request animation frame
     window.requestAnimationFrame { draw(gfx) }
 }
@@ -37,8 +30,8 @@ fun setup() {
 fun draw(gfx: GraphicData) {
     // clear canvas
     gfx.ctx.beginPath()
-    gfx.ctx.fillStyle = "red"
-    gfx.ctx.rect(0.0, 0.0, 100,100)
+    gfx.ctx.fillStyle = "black"
+    gfx.ctx.rect(0.0, 0.0, gfx.width,gfx.height)
     gfx.ctx.fill()
     // request next draw loop
     window.requestAnimationFrame { draw(gfx) }
