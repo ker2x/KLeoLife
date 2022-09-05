93121e0ab6f027fc52414054c8aa435b4c72ffae
Laurent Laborde <kerdezixe@gmail.com>
2022-09-05 22:44:53 +0200

    cleanup

--- src/main/kotlin/kleolife/gl/GLUtils.kt
+++ src/main/kotlin/kleolife/gl/GLUtils.kt
@@ -8,8 +8,6 @@ import org.khronos.webgl.WebGLShader
 class GLUtils {
     companion object {
         fun clearScreen(gfx: GraphicData) {
-            //gfx.ctx.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
-            //gfx.ctx.glClear(GL_COLOR_BUFFER_BIT)
             gfx.ctx.clearColor(0.0f, 0.0f, 0.0f, 1.0f)
             gfx.ctx.clear(WebGLRenderingContext.COLOR_BUFFER_BIT)
         }
@@ -28,7 +26,6 @@ class GLUtils {
             gfx.ctx.linkProgram(program)
             return program
         }
-
     }
 }
 
