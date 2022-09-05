fde0d9e2d5c37a1d45ee6b30a5d2274081205a6d
Laurent Laborde <kerdezixe@gmail.com>
2022-09-05 21:35:10 +0200

    cleanup

--- src/main/kotlin/kleolife/data/GraphicData.kt
+++ src/main/kotlin/kleolife/data/GraphicData.kt
@@ -1,8 +1,9 @@
-import org.w3c.dom.CanvasRenderingContext2D
+package kleolife.data
+
+import org.khronos.webgl.WebGLRenderingContext
 import org.w3c.dom.HTMLCanvasElement
 
 data class GraphicData(var width: Int = 500, var height:Int = 500) {
     lateinit var canvas: HTMLCanvasElement
-    lateinit var ctx: CanvasRenderingContext2D
-
+    lateinit var ctx: WebGLRenderingContext
 }
